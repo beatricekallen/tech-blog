3fde0b6b0734c19bac1795eff3b5221484aaf639
beatricekallen <bkallen90@gmail.com>
2022-05-10 15:06:49 -0400

    sets up models, updates routes and views

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,52 +1,93 @@
-const router = require('express').Router();
-const { Post } = require('../models/');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../models/");
+const withAuth = require("../utils/auth");
 
-router.get('/', withAuth, async (req, res) => {
+//TODO: need to complete and check this route
+router.get("/", withAuth, async (req, res) => {
   try {
     // store the results of the db query in a variable called postData. should use something that "finds all" from the Post model. may need a where clause!
+    Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at",
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          ),
+          "vote_count",
+        ],
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
 
     // this sanitizes the data we just got from the db above (you have to create the above)
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // fill in the view to be rendered
-    res.render('hmmmm what goes here', {
+    res.render("all-posts", {
       // this is how we specify a different layout other than main! no change needed
-      layout: 'dashboard',
+      layout: "dashboard",
       // coming from line 10 above, no change needed
       posts,
     });
   } catch (err) {
-    res.redirect('login');
+    res.redirect("login");
   }
 });
 
-router.get('/new', withAuth, (req, res) => {
+router.get("/new", withAuth, (req, res) => {
   // what view should we send the client when they want to create a new-post? (change this next line)
-  res.render('hmmmm what goes here', {
+  res.render("new-post", {
     // again, rendering with a different layout than main! no change needed
-    layout: 'dashboard',
+    layout: "dashboard",
   });
 });
 
-router.get('/edit/:id', withAuth, async (req, res) => {
+//TODO: complete this route
+router.get("/edit/:id", withAuth, async (req, res) => {
   try {
     // what should we pass here? we need to get some data passed via the request body
-    const postData = await Post.findByPk(????);
+    const postData = await Post.findByPk(req.params.id, {
+      attributes: ["comment_text", "post_url", "user_id"],
+    });
 
     if (postData) {
       // serializing the data
       const post = postData.get({ plain: true });
       // which view should we render if we want to edit a post?
-      res.render('hmmmm what goes here', {
-        layout: 'dashboard',
+      res.render("edit-post", {
+        layout: "dashboard",
         post,
       });
     } else {
       res.status(404).end();
     }
   } catch (err) {
-    res.redirect('login');
+    res.redirect("login");
   }
 });
 
