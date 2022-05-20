c512f86c07d3bb94f2a4e90b238ac1692bfd067b
beatricekallen <bkallen90@gmail.com>
2022-05-20 15:53:57 -0400

    updates user routes, implements login and signup functionality

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,12 +4,10 @@ const { User, Post } = require("../models");
 const userdata = [
   {
     username: "alesmonde0",
-    email: "nwestnedge0@cbc.ca",
     password: "password123",
   },
   {
     username: "jwilloughway1",
-    email: "rmebes1@sogou.com",
     password: "password123",
   },
 ];
